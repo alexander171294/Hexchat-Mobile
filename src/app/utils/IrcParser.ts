00657e1ace93fa4704b062807762347e5637e49f
Alexander Eberle <alexander171294@gmail.com>
2020-06-29 18:28:26 -0300

    parser

--- src/app/utils/IrcParser.ts
+++ src/app/utils/IrcParser.ts
@@ -1,3 +1,22 @@
 export class IRCParser {
-    
+    public static parseMessage(message: string): IRCMessage {
+        const r = /:([^:]+):(.*)/.exec(message);
+        const TAG = r[1];
+        const MSG = r[2];
+        const partials = TAG.split(' ');
+        const im = new IRCMessage();
+        im.origin = partials[0];
+        im.code = partials[1];
+        im.target = partials[2];
+        im.message = MSG;
+        // console.log(TAG, MSG, im);
+        return im;
+    }
+}
+
+export class IRCMessage {
+    public origin: string;
+    public code: string;
+    public target: string;
+    public message: string;
 }
