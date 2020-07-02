c73f4993b32f801886b15a91c71d420fafa992af
Alexander Eberle <alexander171294@gmail.com>
2020-07-01 21:33:03 -0300

    configuracion para deploy.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  gateway: 'ws://irc.network.org:8091/webirc/websocket/',
+  gateway: 'ws://irc.tandilserver.com:8091/webirc/websocket/',
   saveLastMessages: localStorage.getItem('logSize') ? parseInt(localStorage.getItem('logSize')) : 50
 };
