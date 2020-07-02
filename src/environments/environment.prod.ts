3837289806b7497c0ba3bd0bac5d3a19c6b2eb9a
Alexander Eberle <alexander171294@gmail.com>
2020-07-01 21:11:40 -0300

    capasitor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  gateway: 'ws://irc.network.org:8091/webirc/websocket/',
+  saveLastMessages: localStorage.getItem('logSize') ? parseInt(localStorage.getItem('logSize')) : 50
 };
